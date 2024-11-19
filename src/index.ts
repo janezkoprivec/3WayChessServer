f7b8ebb7c6ed66eb2321a8f7411ab0ebddf0ea2f
janezkoprivec <koprivec.janez@gmail.com>
2024-11-19 10:54:54 +0100

    Added socket io initialization

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import express, { Express, Request, Response } from "express";
+import socketIO from "socket.io";
 import dotenv from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+const io = new socketIO.Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
